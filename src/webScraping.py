8414e01aac855eb2eb287b2159acea740cffad41
Fazal ur Rehman <55839006+fazalAgha5@users.noreply.github.com>
2019-12-04 15:19:42 +0500

    Update webScraping.py

--- src/webScraping.py
+++ src/webScraping.py
@@ -7,11 +7,8 @@ CORS(app)
 
 @app.route('/',methods=['GET'])
 def test():
-  #print(getTheAgeNews())
-  data = {"the_age_news": getTheAgeNews(),"the_austrailian_news":getTheAustralianNews()}
+  data = {"the_west":getTheWestNews(),"The_Courier_Mail_News":getTheCourierMailNews(),"the_age_news": getTheAgeNews(),"the_austrailian_news":getTheAustralianNews()}
   print(data)
-  # data = {"the_age_news":{"headlines":["asdfjaksjdfsa f","cvbcvb"],"news":["1321313132231313212","asdfasdfasdfsadf"]},
-  # "the_austrailian_news":{"headlines":["index","asdfjak55"],"news":["aaa132sa3d2f2d","bbbasdfasdfasfd"]}}
   return jsonify(data)
 
 
@@ -22,7 +19,7 @@ def getTheAgeNews():
   soup = BeautifulSoup(page.content, 'html.parser')
   section=soup.find(class_="DF6D8")
   topNews=section.find_all(class_="_1YzQk")
-  heading=[item.find("h3").get_text() for item in topNews];
+  heading=[item.find("h3").get_text() for item in topNews]
   news=[item.find(class_="_3XEsE").get_text() for item in topNews]
   value={"headlines":heading,"news":news}
   return value
@@ -32,7 +29,6 @@ def getTheAustralianNews():
   url="https://www.theaustralian.com.au/"
   page = requests.get(url) 
   soup = BeautifulSoup(page.content, 'html.parser')
-  # section=soup.find(class_="area subarea-1 item  ipos-1 irpos-3")
   heading=[]
   news=[]
   for x in range(8):
@@ -46,9 +42,50 @@ def getTheAustralianNews():
     value={"headlines":heading,"news":news}
   return value
 
+def getTheWestNews():
+  url="https://thewest.com.au/"
+  page = requests.get(url) 
+  soup = BeautifulSoup(page.content, 'html.parser')
+  heading=[]
+  news=[]
+  section=soup.find_all(class_="PortraitCard css-2dzcl1")
+  counter=0
+  for x in section:
+    if(counter==10):
+      break
+    counter+=1
+    heading.append(x.find(class_="Card-HeadlineText css-opujcp").get_text())
+    try:
+      news.append(x.find(class_="Card-Teaser css-1o3aiti").get_text())
+    except:
+      if(len(heading)>0):
+        heading.pop()
+  value={"headlines":heading,"news":news}
+  return value
+
+def getTheCourierMailNews():
+  url="https://www.couriermail.com.au/"
+  page = requests.get(url) 
+  soup = BeautifulSoup(page.content, 'html.parser')
+  heading=[]
+  news=[]
+  section=soup.find_all(class_="tge-cardv2")
+  counter=0
+  for x in section:
+    if(counter==8):
+      break
+    counter+=1
+    h=x.find("h2").get_text()
+    h=h.replace("premium_icon","")
+    heading.append(h)
+    news.append(x.find(class_="tge-cardv2_standfirst").get_text())
+  value={"headlines":heading,"news":news}
+  return value
+
+
+
+
+
 
   if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0',port=5000)
-
-# getTheAgeNews("https://www.theage.com.au")
-#getTheAustralianNews("https://www.theaustralian.com.au/")
+    app.run(host='0.0.0.0',port=5000)
