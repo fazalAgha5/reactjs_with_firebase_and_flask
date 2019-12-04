baa41b97540272c59da2918359d2a4f1cdb7d59d
Fazal ur Rehman <55839006+fazalAgha5@users.noreply.github.com>
2019-12-04 15:20:25 +0500

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -26,12 +26,16 @@ class Home extends React.Component{
         console.log(this.state.data)
         }
         return (
-            <div className="base-container">
+            <div>
                 <button onClick={this.logout} type="btn" >Logout</button>
                 <h2>{this.state.data ==null && "News Loading..."}</h2>
-                <h2>{this.state.data !=null && "The Age news"}</h2>
+                <h2><a href="https://thewest.com.au/">{this.state.data !=null && "The West news"}</a></h2>
+                {this.state.data !=null && this.state.data.the_west['headlines'].map((headline,i) =><div><h3 key={i}>{headline}</h3><p key={i+"a"}>{this.state.data.the_west['news'][i]}</p></div>)}
+                <h2><a href="https://www.couriermail.com.au/">{this.state.data !=null && "The Courier Mail News"}</a></h2>
+                {this.state.data !=null && this.state.data.The_Courier_Mail_News['headlines'].map((headline,i) =><div><h3 key={i}>{headline}</h3><p key={i+"a"}>{this.state.data.The_Courier_Mail_News['news'][i]}</p></div>)}
+                <h2><a href="https://www.theage.com.au">{this.state.data !=null && "The Age news"}</a></h2>
                 {this.state.data !=null && this.state.data.the_age_news['headlines'].map((headline,i) =><div><h3 key={i}>{headline}</h3><p key={i+"a"}>{this.state.data.the_age_news['news'][i]}</p></div>)}
-                <h2>{this.state.data !=null && "The Australian news"}</h2>
+                <h2><a href="https://www.theaustralian.com.au/">{this.state.data !=null && "The Australian news"}</a></h2>
                 {this.state.data !=null && this.state.data.the_austrailian_news['headlines'].map((headline,i) =><div><h3 key={i}>{headline}</h3><p key={i+"a"}>{this.state.data.the_austrailian_news['news'][i]}</p></div>)}
 
             </div>
